9c37be26c8ef4a4b92138571d513ec8c5ce5662b
Wansix <78299639+Wansix@users.noreply.github.com>
2022-07-23 16:47:00 +0000

    main card list 구현

--- src/component/Header.js
+++ src/component/Header.js
@@ -10,15 +10,15 @@ const Header = () => {
       <div className="header__home">
         <a href="/">
           <img src={defaultCatUrl} alt="cat"></img>
-          <h1>Uni-hub</h1>
+          <h1>UNI-HUB</h1>
         </a>
       </div>
       <div className="header__menu">
         <Nav.Link href="/nftInfo">민팅완료</Nav.Link>
         <Nav.Link href="/mintInfo">민팅예정</Nav.Link>
-        <Nav.Link href="/community">게시판</Nav.Link>
-        <Nav.Link href="/news">뉴스</Nav.Link>
-        <Nav.Link href="/notice">공지사항</Nav.Link>
+        <Nav.Link href="/info">정보</Nav.Link>
+        <Nav.Link href="/community">자유</Nav.Link>
+        <Nav.Link href="/hot">Hot</Nav.Link>
         <Nav.Link href="/mint">Mint</Nav.Link>
       </div>
     </div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleArrowRight,
+  faCircleArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Contents = (props) => {
   const list = [];
@@ -23,6 +28,40 @@ const Contents = (props) => {
         <div className="TopNFT-contents__row-content">
           <span>{t.volume}</span>
         </div>
+      </div>
+    );
+  }
+  return <>{list}</>;
+};
+
+const CardContents = (props) => {
+  const list = [];
+
+  for (let i = 0; i < props.cardList.length; i++) {
+    let t = props.cardList[i];
+    list.push(
+      <div key={t.ranking} className="card-content">
+        <a href={t.link}>
+          <div className="card-content__top">
+            <img src={t.img} alt="cardImg"></img>
+          </div>
+
+          <div className="card-content__bottom">
+            <div className="card-content__bottom-projectName">
+              <span>
+                {t.ranking}.{t.name}
+              </span>
+            </div>
+
+            <div className="card-content__bottom-price-wrapper">
+              <span>floor price</span>
+              <div className="card-content__bottom-price-wrapper__price">
+                <img src="https://static.opensea.io/tokens/KLAY.png"></img>
+                <span>{t.floorPrice} klay</span>
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
     );
   }
@@ -77,21 +116,159 @@ const TopNFT = () => {
   );
 };
 
+const TopNFTList = () => {
+  const movePx = 2000;
+  const maxScroll = 2000;
+  const [topNFTProjectList, setTopNFTProjectList] = useState([
+    {
+      ranking: 1,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 2,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 3,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 4,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 5,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 6,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 7,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 8,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 9,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+    {
+      ranking: 10,
+      name: "META KONGZ",
+      img: "https://img.seadn.io/files/0968706385307e193695e2655f5b3689.png?auto=format&fit=max&w=384",
+      floorPrice: 14000,
+      link: "https://themetakongz.com/kr.html",
+    },
+  ]);
+  const [currentPx, setCurrentPx] = useState(0);
+  const [rightScrollpx, setRightScrollpx] = useState(movePx);
+  const [leftScrollpx, setLeftScrollpx] = useState(movePx);
+
+  const cardListRightScroll = (event) => {
+    const viewListPx = window.innerWidth * 0.8;
+    const arrow = event.currentTarget.parentNode;
+
+    let move = currentPx + viewListPx;
+    if (move >= maxScroll) {
+      move = maxScroll;
+    }
+
+    console.log(move);
+
+    arrow.scrollTo({ left: move, top: 0, behavior: "smooth" });
+    setCurrentPx(move);
+  };
+  const cardListLeftScroll = (event) => {
+    const viewListPx = window.innerWidth * 0.8;
+    const arrow = event.currentTarget.parentNode;
+
+    let move = currentPx - viewListPx;
+    if (move <= 0) {
+      move = 0;
+    }
+
+    console.log(move);
+
+    arrow.scrollTo({ left: move, top: 0, behavior: "smooth" });
+    setCurrentPx(move);
+  };
+
+  return (
+    <div className="card-contents">
+      <div
+        className="card-contents__rightArrow cardlist-arrow"
+        onClick={cardListRightScroll}
+      >
+        <FontAwesomeIcon icon={faCircleArrowRight} size="3x" />
+      </div>
+      <div
+        className="card-contents__leftArrow cardlist-arrow"
+        onClick={cardListLeftScroll}
+      >
+        <FontAwesomeIcon icon={faCircleArrowLeft} size="3x" />
+      </div>
+      <CardContents cardList={topNFTProjectList}></CardContents>
+    </div>
+  );
+};
+
+const RankingBoard = () => {
+  return (
+    <div className="rankingBoard">
+      <div className="rankingBoard__header">Top NFT</div>
+      <Tabs
+        defaultActiveKey="topNFT"
+        id="homeTap"
+        className="mb-3"
+        variant="pills"
+      >
+        <Tab eventKey="topNFT" title="Top NFT">
+          <TopNFTList></TopNFTList>
+        </Tab>
+        <Tab eventKey="profile" title="Profile"></Tab>
+        <Tab eventKey="contact" title="Contact"></Tab>
+      </Tabs>
+    </div>
+  );
+};
+
 const Home = () => {
+  console.log("current width", window.innerWidth);
   return (
     <div className="home-wrapper">
       <div className="home-wrapper__header"></div>
-      <Tabs defaultActiveKey="topNFT" id="homeTap" className="mb-3">
-        <Tab eventKey="topNFT" title="Top NFT">
-          <TopNFT></TopNFT>
-        </Tab>
-        <Tab eventKey="profile" title="Profile">
-          {/* <TapContent></TapContent> */}
-        </Tab>
-        <Tab eventKey="contact" title="Contact">
-          {/* <TapContent></TapContent> */}
-        </Tab>
-      </Tabs>
+      <RankingBoard></RankingBoard>
     </div>
   );
 };
